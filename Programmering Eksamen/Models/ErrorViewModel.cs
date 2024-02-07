2f778ac481aaae94702eaf9835e1f63249f0f71c
benj7617 <116954841+benj7617@users.noreply.github.com>
2024-02-07 10:06:32 +0100

    Initial creation of razer files

---
+++ Programmering Eksamen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Programmering_Eksamen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
