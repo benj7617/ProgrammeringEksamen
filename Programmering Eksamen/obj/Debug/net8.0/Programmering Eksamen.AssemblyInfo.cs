6e190e57cc4c5ee534a992cfba92ba40fe4b366b
benj7617 <116954841+benj7617@users.noreply.github.com>
2024-03-06 10:29:48 +0100

    push push push push
    det er push day

--- Programmering Eksamen/obj/Debug/net8.0/Programmering Eksamen.AssemblyInfo.cs
+++ Programmering Eksamen/obj/Debug/net8.0/Programmering Eksamen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Programmering Eksamen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f778ac481aaae94702eaf9835e1f63249f0f71c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89f21c6f42e1b0d1bea09493c58a5f95f2bf821d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Programmering Eksamen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Programmering Eksamen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
