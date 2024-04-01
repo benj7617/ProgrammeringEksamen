387bad6fa01c5e9aafbce7c09dd8df75a0c920ae
Christian Walker Petersen <c.walker.petersen@gmail.com>
2024-04-02 00:33:35 +0200

    ON god

--- Programmering Eksamen/wwwroot/js/fetchAPI.js
+++ Programmering Eksamen/wwwroot/js/fetchAPI.js
@@ -17,16 +17,14 @@ if (isAlredyFetched == null) {
             NameArr = [];
             ImgURLArr = [];
             DescriptionArr = [];
-            AmountArr = [];
-            CostArr = [];
+
             
             for (var i = 0; i < data.drinks.Length(); i++) {
                 DBIDArr.push(data.drinks[i].idDrink);
                 NameArr.push(data.drinks[i].StrDrink);
                 ImgURLArr.push(data.drinks[i].strDrinkThumb);
                 DescriptionArr.push(data.drinks[i].strInstructions);
-                AmountArr.push(0);
-                CostArr.push(0);
+
             }
 
 
